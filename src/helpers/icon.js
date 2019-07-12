2b4716c915780c37df30322ad3718776ab1f10ff
Anton Mayatskiy <worknekki@gmail.com>
2019-07-13 02:46:42 +1000

    Helper for icon created

---
+++ src/helpers/icon.js
@@ -0,0 +1,22 @@
+module.exports = function(options) {
+  const name = options.hash.name;
+  const mods = options.hash.mods;
+  const root = options.data.root.root;
+  let cssClass = 'icon-' + name;
+  let allMods = '';
+
+  if (mods !== 'undefined' && mods ) {
+    const modsList = mods.split(',');
+      for (let i = 0; i < modsList.length; i++) {
+        allMods = allMods + ' icon--' + modsList[i].trim();
+      }
+  }
+
+  // cssClass+= allMods;
+
+  const svg = `<svg class=${cssClass}>
+                 <use xlink:href="${root}assets/img/symbol/sprite.svg#${name}">
+              </svg>`
+
+  return svg;
+}
