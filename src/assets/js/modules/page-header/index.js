1a7c0c7b50bb0a747d3241abb7d38adcbe142c10
Anton Mayatskiy <worknekki@gmail.com>
2019-07-16 00:35:27 +1000

    Update 2019-07-15T14:35:13.551Z

--- src/assets/js/modules/page-header/index.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-
-$('[data-open-user-panel]').on('click', function() {
-  $('html').addClass('page--user-panel-opened');
-});
-
-$('[data-close-user-panel]').on('click', function() {
-  $('html').removeClass('page--user-panel-opened');
-});
