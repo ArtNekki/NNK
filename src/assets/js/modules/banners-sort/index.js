9ab9eaf33bddb9033fc4e9800bdd162fc1a8d35d
Anton Mayatskiy <worknekki@gmail.com>
2019-07-24 20:43:20 +1000

    all structure of pages  refactored

--- src/assets/js/modules/banners-sort/index.js
+++ src/assets/js/modules/banners-sort/index.js
@@ -1,10 +1,10 @@
 import Sortable from 'sortablejs';
 
-new Sortable(document.getElementById('sortBanners'), {
-  animation: 150,
-  chosenClass: 'blue-background-class',
-  // dragClass: 'blue-background-class',
-  // ghostClass: 'blue-background-class'
-});
-
-console.log('sort', Sortable);
+// new Sortable(document.getElementById('sortBanners'), {
+//   animation: 150,
+//   chosenClass: 'blue-background-class',
+//   // dragClass: 'blue-background-class',
+//   // ghostClass: 'blue-background-class'
+// });
+//
+// console.log('sort', Sortable);
