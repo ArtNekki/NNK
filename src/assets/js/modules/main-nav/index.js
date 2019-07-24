f2c3e9a9182c2db0a0c0e8a8ea78a2563ed8ab91
Anton Mayatskiy <worknekki@gmail.com>
2019-07-25 01:26:33 +1000

    Pages in manager section updated

--- src/assets/js/modules/main-nav/index.js
+++ src/assets/js/modules/main-nav/index.js
@@ -1,27 +1,27 @@
-import $ from 'jquery';
-import MainNav from './main-nav';
-
-$(document).ready(function() {
-  const getActiveTabId = function(container) {
-    return $(container).find('.tabs-title.is-active [data-tabs-target]').attr('href');
-  }
-
-  const setActiveTab = function(id) {
-    const tabName = id.slice(1, id.length);
-    $('#main-tabs').foundation('selectTab', tabName);
-  }
-
-  const mainNav = new MainNav({ target: '#main-nav' });
-
-  mainNav.onChange = function(activeLink) {
-    setActiveTab(activeLink.getAttribute('href'));
-  }
-
-  $(document).on('change.zf.tabs', '#main-tabs', function () {
-    const id = getActiveTabId($(this));
-    mainNav.changeActiveItem(id);
-  })
-
-  const activeTabId = getActiveTabId($('#main-tabs'));
-  mainNav.changeActiveItem(activeTabId);
-});
+// import $ from 'jquery';
+// import MainNav from './main-nav';
+//
+// $(document).ready(function() {
+//   const getActiveTabId = function(container) {
+//     return $(container).find('.tabs-title.is-active [data-tabs-target]').attr('href');
+//   }
+//
+//   const setActiveTab = function(id) {
+//     const tabName = id.slice(1, id.length);
+//     $('#main-tabs').foundation('selectTab', tabName);
+//   }
+//
+//   const mainNav = new MainNav({ target: '#main-nav' });
+//
+//   mainNav.onChange = function(activeLink) {
+//     setActiveTab(activeLink.getAttribute('href'));
+//   }
+//
+//   $(document).on('change.zf.tabs', '#main-tabs', function () {
+//     const id = getActiveTabId($(this));
+//     mainNav.changeActiveItem(id);
+//   })
+//
+//   const activeTabId = getActiveTabId($('#main-tabs'));
+//   mainNav.changeActiveItem(activeTabId);
+// });
